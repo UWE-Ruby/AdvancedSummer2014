5efd67e1d01e4bfe4cd7c79902d872f87b6fef48
Sarah <sarahcwheeler@gmail.com>
2014-06-30 00:38:49 -0700

    Works on Week 1 homework, adds Cipher test outlines.

---
+++ week1/homework/solitaire_cipher_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+
+
+describe StepOne do
+	
+	it "should be all caps" do
+		pending
+	end
+
+	it "should have a space after every 5 characters" do
+		pending
+	end
+
+	it "should not have any non-alphabet characters" do
+		pending
+	end
+
+	describe "X handling" do
+		it "should not add Xs if the last group is 5 characters" do
+			pending
+		end
+		it "should add Xs if the last group is < 5 characters" do
+			pending
+		end
+	end
+end
